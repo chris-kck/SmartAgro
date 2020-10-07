9005783292ba97d9577ff21250d7dc57a210fcb5
Chris Kay <kckateera@gmail.com>
2020-10-07 14:53:02 +0200

    Add modules to be automatically imported

--- smartagro/__init__.py
+++ smartagro/__init__.py
@@ -1,4 +1,10 @@
 """Top-level package for SmartAgro."""
+#this file is always run each time the package is imported.
+#will use this to  effect automatic importing of modules. instead of smartagro.smartagro.func()
+
+import smartagro.smartagro, smartagro.utils
+
+#implement as: from smartagro.smartagro import * OR import smatagro then use smartagro.smartagro.func()
 
 __author__ = """Kudzai Chris Kateera"""
 __email__ = 'kckateera@gmail.com'
