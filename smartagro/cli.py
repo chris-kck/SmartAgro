5ff8b300f9312c3830c6c4f14ddc1bd78dc9ddd6
Chris Kay <kckateera@gmail.com>
2020-10-09 22:43:21 +0200

    Change ClI output

--- smartagro/cli.py
+++ smartagro/cli.py
@@ -6,7 +6,7 @@ import click
 @click.command()
 def main(args=None):
     """Console script for smartagro."""
-    click.echo("Replace this message by putting your code into "
+    click.echo("Replace this message by putting your code into ?"
                "smartagro.cli.main")
     click.echo("See click documentation at https://click.palletsprojects.com/")
     return 0
