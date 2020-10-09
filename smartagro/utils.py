9b709f9367e313711cf0762ce62c3d8ee8cbef67
Chris Kay <kckateera@gmail.com>
2020-10-10 01:44:07 +0200

    Adjust code to adhere to PEP8

--- smartagro/utils.py
+++ smartagro/utils.py
@@ -25,22 +25,26 @@ def discover_devices(comm_interface):  # scans address space and ports to discov
     print(os.popen("i2cdetect -y 1").read())
     #TODO Add SPI scanning support
 
-def create_sensor_type(read,write,config): #Add support for new sensor type
+def create_sensor_type(read, write, config):  # Add support for new sensor type
     pass
 
-def sensor_attach_i2c(SensorType1, addr,sample_rate, broker): #Add sensor, assign broker and topic
+def sensor_attach_i2c(SensorType1, addr, sample_rate, broker):  # Add sensor, assign broker and topic
     pass
 
-def sensor__attach_serial(SensorType2, port, baud, broker): #Add sensor, assign broker and topic
+
+def sensor__attach_serial(SensorType2, port, baud, broker):  # Add sensor, assign broker and topic
     pass
 
-def sensor_detach(stream, broker):  #remove sensor from publishing topics
+
+def sensor_detach(stream, broker):  # remove sensor from publishing topics
     pass
 
-def list_active_sensor_streams(broker): #show topics being published to broker
+
+def list_active_sensor_streams(broker):  # show topics being published to broker
     pass
 
-def sensor_update(addr, new_sample_rate): #Dynamic adjustment of sensor details
+
+def sensor_update(addr, new_sample_rate):  # Dynamic adjustment of sensor details
     pass
 
 def find_broker(): #Scan for a MQTT broker within network
