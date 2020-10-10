808444598e4d4d47efd96d250eec154e55b8480e
Chris Kay <kckateera@gmail.com>
2020-10-10 12:01:48 +0200

    Update and refactor code

--- smartagro/utils.py
+++ smartagro/utils.py
@@ -9,6 +9,7 @@ try:
 except RuntimeError:
     print("Error importing RPi.GPIO! Use sudo / run sudo usermod -aG gpio <myusername> to get permission")
 
+
 def bar():
     """
     Useless test function
@@ -17,25 +18,27 @@ def bar():
     from smartagro import __author__
     print('[mod2] bar(){}'.format(__author__))  # f'haha{var}' not supported in python 3.5
 
+
 topic = "SmartAgro/Sensors/"
+
 
 class Bar:
     pass
 
 
-def config_broker(broker="mqtt.eclipse.org", QS=0, PORT="1883", stream_schema="json"):
+def config_broker(broker="mqtt.eclipse.org", QS=0, port="1883", stream_schema="json"):
     """
     Function to configure a new broker to be published to.
     :param broker: The url or ip address of the broker
     :param QS: quality of service determining how many times message is sent. 0,1,2
-    :param PORT: broker port in use. default 1883, ssl 8883
+    :param port: broker port in use. default 1883, ssl 8883
     :param stream_schema: the data stream schema used. Default is json
     :return: No return
     """
-    client = mqtt.Client("RPi0-ZA") #create new client
-    client.connect(broker,PORT) #connect to broker
-    client.publish("dev/test","OFF ua") #TOPIC & test payload
-    #TODO test functionality
+    client = mqtt.Client("RPi0-ZA")  # create new client
+    client.connect(broker, port)  # connect to broker
+    client.publish("dev/test", "OFF ua")  # TOPIC & test payload
+    # TODO test functionality
 
 
 def discover_devices(comm_interface):
@@ -52,8 +55,10 @@ def discover_devices(comm_interface):
     print("Checking if SPI Module is loaded:")
     print(os.popen("lsmod | grep spi").read())
 
+
 def create_sensor_type(read, write, config):  # Add support for new sensor type
     pass
+
 
 def sensor_attach_i2c(SensorType1, addr, sample_rate, broker):  # Add sensor, assign broker and topic
     pass
@@ -72,26 +77,29 @@ def sensor_attach_serial(SensorType2, spi_device, broker, baud=976000):  # Add s
     spi.open(0, spi_device)
     spi.max_speed_hz = 976000
 
-    CLK  = 23
+    CLK = 23
     MISO = 21
     MOSI = 19
-    CS   = 24 if spi_device == 0 else 26 #CE0 or CE1
+    CS = 24 if spi_device == 0 else 26  # CE0 or CE1
 
     GPIO.setup(CLK, GPIO.OUT)
     GPIO.setup(MISO, GPIO.IN)
     GPIO.setup(MOSI, GPIO.OUT)
     GPIO.setup(CS, GPIO.OUT)
 
+
 def sensor_read_analogue(channel):
     """
     Reads an analogue signal from the connected SPI ADC device
     :return: ADC output Normalized with Vref.
     """
-    adc = mcp3008.MCP3008(bus=0, device=0) #link with SPI device initialization. Docs: https://pypi.org/project/mcp3008/
+    # link with SPI device initialization. Docs: https://pypi.org/project/mcp3008/
+    adc = mcp3008.MCP3008(bus=0,device=0)
     ADC_values = adc.read_all()
     print(ADC_values)
     adc.close()
-    return ADC_values[channel+8]
+    return ADC_values[channel + 8]
+
 
 def control_fan(gpio_pin, state):
     """
@@ -101,9 +109,10 @@ def control_fan(gpio_pin, state):
     :return: NO return
     """
     if state:
-        GPIO.output(gpio_pin,GPIO.HIGH)
+        GPIO.output(gpio_pin, GPIO.HIGH)
     else:
-        GPIO.output(gpio_pin,GPIO.HIGH)
+        GPIO.output(gpio_pin, GPIO.HIGH)
+
 
 def sensor_detach(stream, broker):  # remove sensor from publishing topics
     pass
@@ -115,6 +124,7 @@ def list_active_sensor_streams(broker):  # show topics being published to broker
 
 def sensor_update(addr, new_sample_rate):  # Dynamic adjustment of sensor details
     pass
+
 
 def find_broker():
     """
@@ -149,6 +159,7 @@ def get_ip():
         s.close()  # close socket
     return IP
 
+
 def scan_network():
     """
     Scan subnet /24 of IP address to check for brokers on the local network.
@@ -162,22 +173,24 @@ def scan_network():
         dev = ".".join(net) + "." + str(host)
         response = os.popen(f"ping -w 1 {dev}")  # -c 1
         try:
-            if (response.readlines()[5]):
+            if response.readlines()[5]:
                 print(f"{dev} is online")
                 online_dev.append(dev)
-        except:
+        except IndexError:
             continue
     return online_dev
+
 
 def gpio_init():
     """
     Function to initialize the GPIO pins and numbering system used.
     :return: No return
     """
-    GPIO.setmode(GPIO.BOARD) #Physical Pin Numbers
-    #remember to use GPIO.cleanup() and exit(0) for a graceful exit.
+    GPIO.setmode(GPIO.BOARD)  # Physical Pin Numbers
+    # remember to use GPIO.cleanup() and exit(0) for a graceful exit.
 
 # functions to communicate with Seeed devices. ADC/Direct/?
 
 
-# All of the above for actuator //Connecting actuator to raspberry pi, configuring to subscribe to mqtt topics that send commands to activate / deactivate.
+# All of the above for actuator //Connecting actuator to raspberry pi,
+# configuring to subscribe to mqtt topics that send commands to activate / deactivate.
