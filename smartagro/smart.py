356e96d8c30272a968eaac2280c462bd13bb99d5
Chris Kay <kckateera@gmail.com>
2020-10-16 14:08:55 +0200

    Format code according to PEP 8

--- smartagro/smart.py
+++ smartagro/smart.py
@@ -8,8 +8,13 @@ def foo():
 
 
 class SmartAgro:
-    #after searching for a broker
-    # this class instantiates an objecct wc has sensors added to itthen configures one. Next sensors are added with corresponding topics
+    """
+
+    after searching for a broker
+    this class instantiates an object wc has sensors added to it then configures one. N
+    ext sensors are added with corresponding topics
+    """
+
 
     def __init__(self, broker_address="test.mosquitto.org", broker_port=1883, qos=0):
         self.broker_address = broker_address
@@ -17,17 +22,17 @@ class SmartAgro:
         self.qos = qos
         self.sensors = set()
         self.actuators = set()
-        #if none, scan network for brokers and connect to identified broker.
+        # if none, scan network for brokers and connect to identified broker.
         self.config_broker(broker_address, qos, broker_port)
         utils.gpio_init()
         utils.sensor_attach_serial()
 
-    def config_broker(self, broker, qos, port="1883", stream_schema="json"):
+    def config_broker(self, broker, qos, port=1883, stream_schema="json"):
         """
         Function to configure a new broker to be published to.
 
         :param broker: The url or ip address of the broker.
-        :param QS: quality of service determining how many times message is sent. 0,1,2
+        :param qos: quality of service determining how many times message is sent. 0,1,2
         :param port: broker port in use. default 1883, ssl 8883
         :param stream_schema: the data stream schema used. Default is json
         :return: mqtt client object
@@ -41,21 +46,21 @@ class SmartAgro:
 
     # The callback for when a PUBLISH message is received from the server.
     def on_connect(self, client, userdata, flags, rc):
-        print("Connected with result code "+str(rc))
+        print("Connected with result code " + str(rc))
 
     # The callback for when a PUBLISH message is received from the server.
     def on_message(self, client, userdata, msg):
-        print(msg.topic+" "+str(msg.payload))
+        print(msg.topic + " " + str(msg.payload))
 
     def read_sensor(self, channel):
-        #add sensor and add topic
+        # add sensor and add topic
         reading = utils.read_analogue(channel)
         self.sensors.add(channel)
-        self.client.publish(f"smartagro/sensor{channel}", reading) #sensor ID
+        self.client.publish(f"smartagro/sensor{channel}", reading)  # sensor ID
 
     def activate_actuator(self, gpio_pin, state):
         utils.switch_actuator(gpio_pin, state)
         self.actuators.add(gpio_pin)
-        self.client.publish(f"smartagro/actuator{gpio_pin}", state) #actuator ID
+        self.client.publish(f"smartagro/actuator{gpio_pin}", state)  # actuator ID
 
-    #TODO Implement listening of mqtt actuator publish
+    # TODO Implement listening of mqtt actuator publish
