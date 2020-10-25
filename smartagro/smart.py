670778e59f05948443a219d02407ad3a271e539e
Chris Kay <kckateera@gmail.com>
2020-10-25 22:39:20 +0200

    Update method signatures and implementation

--- smartagro/smart.py
+++ smartagro/smart.py
@@ -16,11 +16,8 @@ class SmartAgro:
         """
         Object constructor for the sensors and actuators to be attached to it.
 
-
-    def __init__(self, broker_address="test.mosquitto.org", broker_port=1883, qos=0):
-        self.broker_address = broker_address
-        self.broker_port = broker_port
-        self.qos = qos
+        """
+        self.client = None
         self.sensors = set()
         self.actuators = set()
         self.dhtDevice = adafruit_dht.DHT11(18) #GPIO 18 (Physical 12)
@@ -43,8 +40,10 @@ class SmartAgro:
         self.client = mqtt.Client("RPi0-ZA-2020")  # create new client
         self.client.connect(broker, port)  # connect to broker
         self.client.publish(topic="smartagro/test", payload="Test Successful", qos=qos)  # TOPIC & test payload
+        #self.client.subscribe("smartagro/#")
         self.client.on_connect = self.on_connect
         self.client.on_message = self.on_message
+        self.client.loop_start()
 
     def on_connect(self, client, userdata, flags, rc):
         """
@@ -70,22 +69,50 @@ class SmartAgro:
         pass #print(msg.topic + " " + str(msg.payload))
 
     def read_sensor(self, channel):
-        reading = utils.read_analogue(channel)
-        self.client.publish(f"smartagro/sensor/{channel}", reading)  # sensor ID
-        self.sensors.add(f"smartagro/sensor/{channel}")
+        """
+        # Reads sensor, publishes topic to broker
+        :param channel:
+        :return:
+        """
+        reading = utils.read_analogue(channel)/7
+        self.client.publish(f"smartagro/sensor/{'Moisture' if channel==0 else 'Light'}", reading)  # sensor ID
+        self.sensors.add(f"smartagro/sensor/{'Moisture' if channel==0 else 'Light'}")
+        return reading
 
-    def read_dht_11(self,pin=17):
-        sensor=11 #DHT11 sensor
-        humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+    def get_dht(self):
+        """
+        A function to get readings from the single wire DHT11 device.
+
+        :param gpio: GPIO Pin connected to DHT Data Pin Default: GPIO 18 (Physical 12)
+        :return: Temperature and Humidity Readings
+        """
+        try:
+            temp=humidity=0
+            temp, humidity = self.dhtDevice.temperature, self.dhtDevice.humidity
+        except RuntimeError as error:
+            print(error.args[0])
+            time.sleep(2.0) # Retry after 2 seconds
+            temp, humidity = self.dhtDevice.temperature, self.dhtDevice.humidity
+        except Exception as error:
+            self.dhtDevice.exit()
+            raise error
+        finally:
+            print(f"Temp: {round(temp,2)} C &  Humidity: {round(humidity,2)}% ")  # TODO REMOVE LINE
+            yield temp, humidity
+
+    def read_dht(self):
+        temperature, humidity = next(self.get_dht())
         self.client.publish(f"smartagro/sensor/humidity", humidity)
         self.client.publish(f"smartagro/sensor/temperature", temperature)
         self.sensors.add(f"smartagro/sensor/humidity")
         self.sensors.add(f"smartagro/sensor/temperature")
+        return temperature, humidity
 
     def read_all(self):
-        for channel in range(3):
-            self.read_sensor(channel)
-        self.read_dht_11()
+        moist = self.read_sensor(0)  # Moisture
+        light = self.read_sensor(2)  # Light
+        temp, humid = self.read_dht()  # DHT
+        return moist, light, temp, humid
 
     def activate_actuator(self, gpio_pin, state):
         utils.switch_actuator(gpio_pin, state)
@@ -98,9 +125,8 @@ class SmartAgro:
         except KeyError:
             print("The device you are trying to remove des not exist. Check connected devices again!")
 
-
     def active_devices(self):
-        return (self.actuators | self.sensors)
+        return self.actuators | self.sensors
 
 # TODO Implement listening of mqtt actuator publish
 # TODO add Keyboard interrupt catch for graceful exit.
