6316daf0a9b66963666885d5eee678a16aaa509f
Chris Kay <kckateera@gmail.com>
2020-10-25 23:25:47 +0200

    Update docs and docs conf

--- docs/conf.py
+++ docs/conf.py
@@ -32,7 +32,7 @@ sys.path.insert(0, os.path.abspath('../'))
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode', 'sphinx_rtd_theme']
-autodoc_mock_imports = ["RPi", "paho", "mcp3008", "socket", "spidev", "Adafruit_DHT", ]
+autodoc_mock_imports = ["RPi", "paho", "mcp3008", "socket", "spidev", "Adafruit_DHT", "adafruit_dht"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
--- docs/installation.rst
+++ docs/installation.rst
@@ -20,7 +20,7 @@ This is the preferred method to install SmartAgro, as it will always install the
 If you don't have `pip3`_ installed, this `Python installation guide`_ can guide
 you through the process.
 
-.. _pip: https://pip.pypa.io
+.. _pip3: https://pip.pypa.io
 .. _Python installation guide: http://docs.python-guide.org/en/latest/starting/installation/
 
 
